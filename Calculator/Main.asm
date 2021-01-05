f83be50e966f3b9c7b49e0cc94c72ae649d0d2cd
Maha-Elkomey <47718954+MahaElomey@users.noreply.github.com>
2021-01-05 14:52:21 +0200

    default update comments

--- Calculator/Main.asm
+++ Calculator/Main.asm
@@ -16,60 +16,60 @@ aux    DWORD 0            ; store the number that will be multiplied or divided
 main PROC
 
 start : 
-  mov sign,1
-  mov result,0
-  mov edx, offset msg        ;get the offest of the variable msg
-  call WriteString           ;print the text in the variable msg to the user
-  mov edx, offset buffer     ;get the offest of buffer
-  mov ecx, sizeof buffer     ;put the size of the array in register ecx
-  call ReadString            ;take input from user
-  mov ebp,0                  ;put zero to ebp register ( index of the buffer array )
+             mov sign,1
+             mov result,0
+             mov edx, offset msg        ;get the offest of the variable msg
+             call WriteString           ;print the text in the variable msg to the user
+             mov edx, offset buffer     ;get the offest of buffer
+             mov ecx, sizeof buffer     ;put the size of the array in register ecx
+             call ReadString            ;take input from user
+             mov ebp,0                  ;put zero to ebp register ( index of the buffer array )
 
-loop1 :                      ;while loop
-  cmp ebp, ecx               ;compare ebp  register (index) with ecx register (string length)
-  jae answer                 ;if (ebp >= ecx) then jump to label answer
+loop1 :                                 ;while loop
+             cmp ebp, ecx               ;compare ebp  register (index) with ecx register (string length)
+             jae answer                 ;if (ebp >= ecx) then jump to label answer
   
 digit :  
-  cmp buffer[ebp],'0'
-  jb switch                  ;jump to switch label if this char is less than cahr '0'
-  cmp buffer[ebp],'9'
-  ja switch                  ;jump to switch label if this char is greater than cahr '9' 
-  mov bl,buffer[ebp]         ;store the char in ith index(ebp) into bl "char is BYTE" 
-  sub bl,'0'                 ;convert char number to int number (v[i]-'0')
-  movzx ebx, bl              ;convert 8-bit register to 32-bit(to adding the same size)
-  mov ax ,10                 ;ax = 10
-  mul nr                     ;eax = ax*nr => eax = 10*nr
-  add eax , ebx              ;eax = eax+ebx => eax = 10*nr + (v[i]-'0')
-  mov nr , eax               ;nr=nr*10 + (v[i]-'0')
-  inc ebp                    ;i++ => ebp++
-  jmp digit                  ;jump to get the second number like (123)
+             cmp buffer[ebp],'0'
+             jb switch                  ;jump to switch label if this char is less than cahr '0'
+             cmp buffer[ebp],'9'
+             ja switch                  ;jump to switch label if this char is greater than cahr '9' 
+             mov bl,buffer[ebp]         ;store the char in ith index(ebp) into bl "char is BYTE" 
+             sub bl,'0'                 ;convert char number to int number (v[i]-'0')
+             movzx ebx, bl              ;convert 8-bit register to 32-bit(to adding the same size)
+             mov ax ,10                 ;ax = 10
+             mul nr                     ;eax = ax*nr => eax = 10*nr
+             add eax , ebx              ;eax = eax+ebx => eax = 10*nr + (v[i]-'0')
+             mov nr , eax               ;nr=nr*10 + (v[i]-'0')
+             inc ebp                    ;i++ => ebp++
+             jmp digit                  ;jump to get the second number like (123)
   
 switch : 
-   cmp buffer[ebp],'+'        
-   je addition               ;jump if v[i]=='+'
-   cmp buffer[ebp],'-'
-   je subtraction            ;jump if v[i]=='-'
-   cmp buffer[ebp],'*'
-   je multiplication         ;jump if v[i]=='*'
-   cmp buffer[ebp],'/'
-   je Division               ;jump if v[i]=='/'
+             cmp buffer[ebp],'+'        
+             je addition               ;jump if v[i]=='+'
+             cmp buffer[ebp],'-'
+             je subtraction            ;jump if v[i]=='-'
+             cmp buffer[ebp],'*'
+             je multiplication         ;jump if v[i]=='*'
+             cmp buffer[ebp],'/'
+             je Division               ;jump if v[i]=='/'
 
 
  addition : 
-              cmp buffer[ebp-1],'*'            
-              je psign                        ;jump  to label psing if buffer[ebp] == '*'
-              cmp buffer[ebp-1],'/'
-              je psign                        ;jump  to label psing if buffer[ebp] == '/'
-              cmp co, 1
-              jne aco2                        ;jump  to label aco2 if buffer[ebp] != 1 
-              mov eax,sign                   
-              mul nr                          ;nr * eax (sign)
-              mul aux                         ;eax * aux
-              add result,eax                  ;result += eax
-              mov aux, 0
-              mov nr, 0
-              mov co, 0
-              jmp psign
+             cmp buffer[ebp-1],'*'            
+             je psign                        ;jump  to label psing if buffer[ebp] == '*'
+             cmp buffer[ebp-1],'/'
+             je psign                        ;jump  to label psing if buffer[ebp] == '/'
+             cmp co, 1
+             jne aco2                        ;jump  to label aco2 if buffer[ebp] != 1 
+             mov eax,sign                   
+             mul nr                          ;nr * eax (sign)
+             mul aux                         ;eax * aux
+             add result,eax                  ;result += eax
+             mov aux, 0
+             mov nr, 0
+             mov co, 0
+             jmp psign
 
   subtraction :
              cmp buffer[ebp - 1], '*'
@@ -88,45 +88,29 @@ switch :
              mov co, 0
              jmp nsign
 
-multiplication :    cmp aux, 0                     
-                    jne mco2             ;jump to label mco2 if aux != 0
-                    mov eax,sign         ;eax = 1 or eax = -1 => sign may be 1 or -1
-                    mul nr               ;nr*eax(sign)
-                    mov aux , eax        ;aux = eax 
-                    mov co  , 1          ;set co = 1
-                    mov nr ,0            ;set nr = 0
-                    jmp psign            ;jump to label psign
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+multiplication :    
+             cmp aux, 0                     
+             jne mco2                       ;jump to label mco2 if aux != 0
+             mov eax,sign                   ;eax = 1 or eax = -1 => sign may be 1 or -1
+             mul nr                         ;nr*eax(sign)
+             mov aux , eax                  ;aux = eax 
+             mov co  , 1                    ;set co = 1
+             mov nr ,0                      ;set nr = 0
+             jmp psign                      ;jump to label psign
                     
                     
                     
                     
                     
 Division : 
-            cmp aux ,0       
-            jne div1         ;jump to label div1 if aux != 0
-            mov eax,sign     ;eax = sign => sign may be 1 or -1
-            mul nr           ;nr*eax(sign)
-            mov aux ,eax     ;aux = eax
-            mov co  , 2      ;set co = 2
-            mov nr ,0        ;set nr = 0
-            jmp psign        ;jump to label psign
+             cmp aux ,0       
+             jne div1                        ;jump to label div1 if aux != 0
+             mov eax,sign                    ;eax = sign => sign may be 1 or -1
+             mul nr                          ;nr*eax(sign)
+             mov aux ,eax                    ;aux = eax
+             mov co  , 2                     ;set co = 2
+             mov nr ,0                       ;set nr = 0
+             jmp psign                       ;jump to label psign
               
               
   default : 
@@ -142,17 +126,19 @@ Division :
              add result,eax                  ; resule+= eax , result += nr * aux * sign
              jmp answer
              
-   dco2 :    cmp co, 2
-             jne dco0
-             mov eax,aux
-             div nr
-             mul sign
-             add result,eax
+   dco2 :    
+             cmp co, 2
+             jne dco0                        ;jump if c!=2
+             mov eax,aux                     ;eax = aux
+             div nr                          ;eax = aux / nr
+             mul sign                        ;eax = aux / nr * sign
+             add result,eax                  ;result += aux / nr * sign
              jmp answer
 
-    dco0 :   mov eax, sign
-             mul nr
-             add result,eax
+    dco0 :  
+             mov eax, sign                   ;eax = sign
+             mul nr                          ;eax = sign * nr
+             add result,eax                  ;result += sign * nr
              jmp answer
 
 
