fa815a602afd8fb493f8d7d5f974ca84a6456f23
Maha-Elkomey <47718954+MahaElomey@users.noreply.github.com>
2021-01-06 17:40:23 +0200

    start label

--- Calculator/Main.asm
+++ Calculator/Main.asm
@@ -16,6 +16,7 @@ aux    DWORD 0            ; store the number that will be multiplied or divided
 main PROC
 
 start : 
+             mov ebp,0                  ;put zero to ebp register ( index of the buffer array )
              mov sign,1
              mov result,0
              mov edx, offset msg        ;get the offest of the variable msg
@@ -23,8 +24,7 @@ start :
              mov edx, offset buffer     ;get the offest of buffer
              mov ecx, sizeof buffer     ;put the size of the array in register ecx
              call ReadString            ;take input from user
-             mov ebp,0                  ;put zero to ebp register ( index of the buffer array )
-
+            
 loop1 :                                 ;while loop
              cmp ebp, ecx               ;compare ebp  register (index) with ecx register (string length)
              jae answer                 ;if (ebp >= ecx) then jump to label answer
